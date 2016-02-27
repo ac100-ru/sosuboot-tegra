ab44f19498f03f2d4267418bc0fb6e4d0802282b
Misha Komarovskiy <zombah@gmail.com>
2016-02-27 21:44:19 +0300

    post_image: Add version to result image name
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- board/tegra20/paz00/post_image.sh
+++ board/tegra20/paz00/post_image.sh
@@ -4,7 +4,7 @@
 #author      : zombah
 #date        : 24/02/2016
 #version     : 0.1
-#usage       : post_image.sh <BINARIES_DIR> <BR2_EXTERNAL>
+#usage       : post_image.sh <BINARIES_DIR> <BR2_EXTERNAL> <BR2_VERSION_FULL>
 #--------------------------------------------------------------------------------
 
 die() {
@@ -12,9 +12,10 @@ die() {
     exit 1
 }
 
-[ 2 -ne $# ] && die "invalid number of parameters $# (expected 2)"
+[ 3 -ne $# ] && die "invalid number of parameters $# (expected 3)"
 
 IMAGE_DIR=$1
 BOARD_DIR=$2
+VERSION=$3
 
-perl $BOARD_DIR/board/tegra20/paz00/create_image.pl $IMAGE_DIR/u-boot-dtb-tegra.bin $IMAGE_DIR/zImage $IMAGE_DIR/rootfs.cpio.uboot $IMAGE_DIR/tegra20-paz00.dtb $BOARD_DIR/board/tegra20/paz00/rcm_boot.cmd $IMAGE_DIR/sos-uboot.bin
+perl $BOARD_DIR/board/tegra20/paz00/create_image.pl $IMAGE_DIR/u-boot-dtb-tegra.bin $IMAGE_DIR/zImage $IMAGE_DIR/rootfs.cpio.uboot $IMAGE_DIR/tegra20-paz00.dtb $BOARD_DIR/board/tegra20/paz00/rcm_boot.cmd $IMAGE_DIR/sos-uboot-$VERSION.bin

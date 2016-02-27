be929cbb19a72f053968771d879fbd8284d34a44
Misha Komarovskiy <zombah@gmail.com>
2016-02-27 23:16:56 +0300

    packages: Add ac100-self-installer scripts
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

---
+++ packages/ac100-self-installers/ac100-self-installers.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# ac100-self-installers
+#
+################################################################################
+
+AC100_SELF_INSTALLERS_VERSION = v1
+AC100_SELF_INSTALLERS_GITHUB_VERSION = ng
+AC100_SELF_INSTALLERS_SITE = $(call github,Stuw,ac100-self-installers,$(AC100_SELF_INSTALLERS_GITHUB_VERSION))
+AC100_SELF_INSTALLERS_LICENSE = GPLv3
+AC100_SELF_INSTALLERS_LICENSE_FILES = COPYING
+AC100_SELF_INSTALLERS_DEPENDENCIES = host-makeself
+
+define AC100_SELF_INSTALLERS_BUILD_CMDS
+       $(HOST_DIR)/usr/bin/makeself.sh $(@D)/switch-to-uboot $(@D)/paz00-switch-to-uboot "AC100 uboot+gpt installer" ./main.sh
+       $(HOST_DIR)/usr/bin/makeself.sh $(@D)/partitioner $(@D)/paz00-partitioner "AC100 uboot Android + Linux partitioner" ./main.sh
+endef
+
+define AC100_SELF_INSTALLERS_INSTALL_TARGET_CMDS
+       $(INSTALL) -m 0755 $(@D)/paz00-switch-to-uboot $(TARGET_DIR)/usr/bin/switch-to-uboot
+       $(INSTALL) -m 0755 $(@D)/paz00-partitioner $(TARGET_DIR)/usr/bin/partitioner
+endef
+
+$(eval $(generic-package))

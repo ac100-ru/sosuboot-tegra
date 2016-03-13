058209c8ccce1ab89108b676972bdb681dedbc57
Misha Komarovskiy <zombah@gmail.com>
2016-03-13 05:16:02 +0300

    Add script to create config with mainline kernel
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

---
+++ linux_custom.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+LINUX_BRANCH=mainline
+LINUX_BANNER=https://www.kernel.org/finger_banner
+LINUX_VERSION=$(wget -qO- $LINUX_BANNER | awk -v pattern=$LINUX_BRANCH 'match($0, pattern) {print $(NF)}' |head -1)
+
+sed -e 's,@LINUX_VERSION,'"$LINUX_VERSION"',' configs/tegra20-paz00_temp_defconfig > configs/tegra20-paz00_mainline_defconfig

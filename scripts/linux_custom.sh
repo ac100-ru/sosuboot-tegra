1e381b1ab5e97c3197bc32a0b83ce3c62d222afa
Misha Komarovskiy <zombah@gmail.com>
2016-03-14 15:06:16 +0300

    scripts: Move linux-custom.sh to scripts dir
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- scripts/linux_custom.sh
+++ scripts/linux_custom.sh
@@ -3,4 +3,4 @@ LINUX_BRANCH=mainline
 LINUX_BANNER=https://www.kernel.org/finger_banner
 LINUX_VERSION=$(wget -qO- $LINUX_BANNER | awk -v pattern=$LINUX_BRANCH 'match($0, pattern) {print $(NF)}' |head -1)
 
-sed -e 's,@LINUX_VERSION,'"$LINUX_VERSION"',' configs/tegra20-paz00_temp_defconfig > configs/tegra20-paz00_mainline_defconfig
+sed -e 's,@LINUX_VERSION,'"$LINUX_VERSION"',' ../configs/tegra20-paz00_temp_defconfig > ../configs/tegra20-paz00_mainline_defconfig

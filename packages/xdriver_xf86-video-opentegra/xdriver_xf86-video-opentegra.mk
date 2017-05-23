3734aa86ed034ad40cf83a70db8920e7288d4a3f
Misha Komarovskiy <zombah@gmail.com>
2017-05-23 16:58:42 +0300

    package: opentegra: Update to latest master and add xv
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- packages/xdriver_xf86-video-opentegra/xdriver_xf86-video-opentegra.mk
+++ packages/xdriver_xf86-video-opentegra/xdriver_xf86-video-opentegra.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_OPENTEGRA_VERSION = 8b3791ddcc50494154a649d4f3eed438e2beadc0
+XDRIVER_XF86_VIDEO_OPENTEGRA_VERSION = 29c7a7ea214ccdc05da97a7109c0f72f93bedb43
 XDRIVER_XF86_VIDEO_OPENTEGRA_SITE = $(call github,grate-driver,xf86-video-opentegra,$(XDRIVER_XF86_VIDEO_OPENTEGRA_VERSION))
 XDRIVER_XF86_VIDEO_OPENTEGRA_LICENSE = MIT
 XDRIVER_XF86_VIDEO_OPENTEGRA_LICENSE_FILES = COPYING
@@ -12,13 +12,14 @@ XDRIVER_XF86_VIDEO_OPENTEGRA_DEPENDENCIES = \
         xserver_xorg-server \
         xproto_xproto \
         xproto_randrproto \
+        xproto_videoproto \
         xproto_xf86driproto \
         libdrm
 XDRIVER_XF86_VIDEO_OPENTEGRA_AUTORECONF = YES
 
 # Autoreconf requires an existing m4 directory
 define XDRIVER_XF86_VIDEO_OPENTEGRA_PATCH_M4
-	mkdir -p $(@D)/m4
+        mkdir -p $(@D)/m4
 endef
 XDRIVER_XF86_VIDEO_OPENTEGRA_POST_PATCH_HOOKS += XDRIVER_XF86_VIDEO_OPENTEGRA_PATCH_M4
 
